c25f8be8c72eb6bc81b2faa4835fe9184653a3f0
JFlores01 <juan.floree60@gmail.com>
2022-11-14 20:40:37 +0100

    Subida con foto logo

--- Validación_Avanzada_JS/js/myscript.js
+++ Validación_Avanzada_JS/js/myscript.js
@@ -27,15 +27,15 @@ var caja_trasera_register = document.querySelector(".caja_trasera-register");
 }*/
 
 
-function login(){
+function login() {
 
-    if(window.innerWidth > 840){
+    if (window.innerWidth > 840) {
         formulario_register.style.display = "none";
-    contenedor_login_register.style.left = "10px";
-    formulario_login.style.display = "block";
-    caja_trasera_register.style.opacity = "1";
-    caja_trasera_login.style.opacity = "0";
-    }else{
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    } else {
 
         formulario_register.style.display = "none";
         contenedor_login_register.style.left = "0px";
@@ -44,27 +44,27 @@ function login(){
         caja_trasera_login.style.display = "none";
 
     }
-   
+
 
 
 }
 
-function register(){
-if(window.innerWidth > 850){
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
-}else{
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "0px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.display = "none";
-    caja_trasera_login.style.display = "block";
-    caja_trasera_login.style.opacity = "1";
-}
-    
+function register() {
+    if (window.innerWidth > 840) {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    } else {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+    }
+
 
 
 }
